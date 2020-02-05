3821fe714785814eecaf779b40b2e0ed8a3e8cb8
Tabinda Sarwar <51069576+sarwart@users.noreply.github.com>
2020-02-05 13:39:35 +1100

    Delete cal_angle_3d.m

--- cal_angle_3d.m
+++
@@ -1,6 +0,0 @@
-function theta=cal_angle_3d(v1,v2)
-
-theta=real(acosd(((v1(1)*v2(1))+(v1(2)*v2(2))+(v1(3)*v2(3)))/(sqrt(v1(1)^2+v1(2)^2+v1(3)^2)*sqrt(v2(1)^2+v2(2)^2+v2(3)^2))));
-end              
-
-                
