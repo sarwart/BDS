5fb1932edc122882702bb5a221717d2cb17f6108
Tabinda Sarwar <51069576+sarwart@users.noreply.github.com>
2020-02-08 12:59:35 +1100

    Update demo_BI.m

--- demo_BI.m
+++ demo_BI.m
@@ -3,7 +3,7 @@ clear;
 close all;
 
 %add the scripts to current path
-addpath(strcat(pwd,'\tck_to_block'));
+addpath(strcat(pwd,'\strm_to_block'));
 
 %%%%%%%%%%Block specifications%%%%%%%%%%%%%%%%
 %specifiy size of block
@@ -18,21 +18,40 @@ N_core=4;
 %%%%%%%%%%%dMRI signal and its corresponding track file%%%%%%%%%%%%%%%%
 disp('Loading dataset...');
 tic
-%provide tck file path
-track_file=strcat(pwd,'\data\ground.tck');
-tracks = read_mrtrix_tracks(track_file);
+
 %provide respective dMRI data or atlas for dimension extraction,
 %which was also used for extracting raw dMRI blocks for training CNN
-dmri_file=strcat(pwd,'\data\ground.nii.gz');
+dmri_file=strcat(pwd,'\Data\ground.nii.gz');
 info = niftiinfo(dmri_file);
 toc
+%provide tck file path
+track_file=strcat(pwd,'\Data\ground.tck');
+
+
+%for tck file
+tracks = read_mrtrix_tracks(track_file);
+streamlines= cell(1,length(tracks.data));
+for i=1:length(tracks.data)
+streamlines(i)=tracks.data(i);
+end
+clear tracks;
+
+%for trk file
+%{
+[header, tracks]=trk_read(track_file);
+streamlines= cell(1,length(tracks));
+for i=1:length(tracks)
+streamlines{i}=tracks(i).matrix;
+end
+clear tracks;
+%}
 
 %%%%%%Generate block-image from the provided tracks and dMRI data%%%%%%%%%%%% 
 
 %generate block_image
 disp('Generating Block-image...');
 tic
-conn_map=generate_block_image(tracks,info,atlas,N_core);
+conn_map=generate_block_image(streamlines,info,atlas,N_core);
 toc
 %%%%%%%%%%%%%%save block_image%%%%%%%%%%%%%%%%%%%%%%%%%
 save_path=strcat(pwd,'\data\block_image.mat');
